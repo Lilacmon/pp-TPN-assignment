9c0074a6e677d9a57d5f434ce669f9e5379e75f6
Lilacmon <70576995+Lilacmon@users.noreply.github.com>
2020-09-06 19:52:35 +0800

    Update simple_spatial_temporal_modules.py

--- models2/spatial_temporal_modules/simple_spatial_temporal_modules.py
+++ models2/spatial_temporal_modules/simple_spatial_temporal_modules.py
@@ -1,7 +1,5 @@
 import paddle.fluid as fluid
 import numpy as np
-import pdb
-
 
 class SimpleSpatialTemporalModule():
     def __init__(self, spatial_type='avg', spatial_size=7, temporal_size=1):
@@ -18,14 +16,3 @@ class SimpleSpatialTemporalModule():
             return fluid.layers.pool3d(input=input, pool_size=self.pool_size, pool_type='avg',
                 pool_stride=1, pool_padding=0)
 
-
-if __name__ == '__main__':
-    spatial_temporal_module=dict(
-                        spatial_type='avg',
-                        temporal_size=1,
-                        spatial_size=7)
-    a = SimpleSpatialTemporalModule(**spatial_temporal_module)
-    data_shape = [2048, 1, 7, 7]
-    img = fluid.layers.data(name='images', shape=data_shape, dtype='float32')
-    x = a.net(img)
-    print(x.shape) #[2, 2048, 1, 1, 1]
